603b048a552c96dc9d5303a5e6869b47f21bb3b9
kangkaisen <kangkaisen@apache.org>
2025-11-30 17:19:06 +0800

    Release 0.1.11 version

--- docs/lib/download-config.ts
+++ docs/lib/download-config.ts
@@ -1,7 +1,7 @@
 export const DOWNLOAD_CONFIG = {
   // Current version
-  version: '0.1.10',
-  releaseDate: '2025-11-28',
+  version: '0.1.11',
+  releaseDate: '2025-11-30',
 
   // GitHub links
   github: {
@@ -12,7 +12,7 @@ export const DOWNLOAD_CONFIG = {
   },
 
   downloads: {
-    'darwin-aarch64': 'https://cdn.talkcody.com/releases/v0.1.10/TalkCody_0.1.10_aarch64.dmg',
+    'darwin-aarch64': 'https://cdn.talkcody.com/releases/v0.1.11/TalkCody_0.1.11_aarch64.dmg',
     'darwin-x86_64': 'https://cdn.talkcody.com/releases/v0.1.10/TalkCody_0.1.10_x64.dmg',
     'windows-x86_64': 'https://cdn.talkcody.com/releases/v0.1.9/TalkCody_0.1.9_x64_en-US.msi',
     'linux-x86_64': 'https://cdn.talkcody.com/releases/v0.1.9/TalkCody_0.1.9_amd64.AppImage',
